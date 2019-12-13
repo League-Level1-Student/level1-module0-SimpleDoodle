a365b4ac3ed0eba33e6a717c7065a8dc70507d41
League Student <league@iMac-25.attlocal.net>
2019-12-12 18:25:00 -0800

    :sunglasses:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,25 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
+		Robot rob = new Robot();
+		rob.penDown();
+		rob.setSpeed(10);
+		rob.setX(10);
+		rob.setY(600);
+		rob.setPenWidth(5);
+if (0==0) {
+	rob.move(60);
+	rob.turn(60);
+	rob.move(30);
+	rob.turn(60);
+	rob.move(30);
+	rob.turn(60);
+	rob.move(60);
+}
+			
 		
 	}
 }
