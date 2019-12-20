fc9a7eb1fc394686d7bf524a51cf20bd2e8d338b
League Student <league@iMac-9.attlocal.net>
2019-12-19 18:29:11 -0800

    :sunglasses:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -5,24 +5,57 @@ import javax.swing.JOptionPane;
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
+	Robot rob = new Robot();
 	public void run() {
-		Robot rob = new Robot();
 		rob.penDown();
 		rob.setSpeed(10);
 		rob.setX(10);
 		rob.setY(600);
 		rob.setPenWidth(5);
-		JOptionPane.showInputDialog(null, "");
-if (0==0) {
-	rob.move(60);
-	rob.turn(60);
-	rob.move(30);
-	rob.turn(60);
-	rob.move(30);
-	rob.turn(60);
-	rob.move(60);
-}
-			
-		
+		JOptionPane.showInputDialog(null, "Do you want the house to be small, medium, or large?");
+		if (0 == 0) {
+			rob.move(100);
+			rob.turn(60);
+			rob.move(30);
+			rob.turn(60);
+			rob.move(30);
+			rob.turn(60);
+			rob.move(100);
+		}
+
 	}
+	void makeHouseSmall(String size){
+		if (0==0) {
+			rob.move(60);
+			rob.turn(60);
+			rob.move(30);
+			rob.turn(60);
+			rob.move(30);
+			rob.turn(60);
+			rob.move(60);
+		}
+		}
+	void makeHouseMedium(String size){
+		if (0==0) {
+			rob.move(120);
+			rob.turn(60);
+			rob.move(30);
+			rob.turn(60);
+			rob.move(30);
+			rob.turn(60);
+			rob.move(120);
+		}
+		}
+	void makeHouseLarge(String size){
+		if (0==0) {
+			rob.move(250);
+			rob.turn(60);
+			rob.move(30);
+			rob.turn(60);
+			rob.move(30);
+			rob.turn(60);
+			rob.move(250);
+		}
+		}
+
 }
