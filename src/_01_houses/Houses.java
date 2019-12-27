247f2760d3525dc638a182e6fc1c2432e938a2cc
League Student <league@iMac-9.attlocal.net>
2019-12-26 18:28:10 -0800

    :rat:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -6,56 +6,146 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 	Robot rob = new Robot();
+int roof = 60;
+int y = 0;
 	public void run() {
 		rob.penDown();
 		rob.setSpeed(10);
 		rob.setX(10);
 		rob.setY(600);
 		rob.setPenWidth(5);
-		JOptionPane.showInputDialog(null, "Do you want the house to be small, medium, or large?");
-		if (0 == 0) {
-			rob.move(100);
-			rob.turn(60);
-			rob.move(30);
-			rob.turn(60);
-			rob.move(30);
-			rob.turn(60);
-			rob.move(100);
+		rob.move(100);
+		rob.turn(roof);
+		rob.move(30);
+		rob.turn(roof);
+		rob.move(30);
+		rob.turn(60);
+		rob.move(100);
+		rob.setPenColor(25, 98, 1);
+		rob.turn(-90);
+		rob.move(40);
+		rob.turn(-90);
+		rob.setPenColor(0, 0, 0);
+
+		for (int i = 0; i < 9; i++) {
+			String size = JOptionPane.showInputDialog(null, "Do you want the house to be small, medium, or large?");
+			String colour = JOptionPane.showInputDialog(null,"What colour do you want the house to be? Red, orange, yellow, green, blue, or purple?");
+			String roofshape = JOptionPane.showInputDialog(null, "Do you want the roof to be pointy or flat?");
+			if (roofshape.equalsIgnoreCase("pointy")) {
+				pointyRoof();
+			}
+			if (roofshape.equalsIgnoreCase("flat")) {
+				flatRoof();
+			}
+			if (colour.equalsIgnoreCase("red")) {
+				red();
+			}
+			if (colour.equalsIgnoreCase("orange")) {
+				orange();
+			}
+			if (colour.equalsIgnoreCase("yellow")) {
+				yellow();
+			}
+			if (colour.equalsIgnoreCase("green")) {
+				green();
+			}
+			if (colour.equalsIgnoreCase("blue")) {
+				blue();
+			}
+			if (colour.equalsIgnoreCase("purple")) {
+				purple();
+			}
+			if (size.equalsIgnoreCase("small")) {
+				makeHouseSmall();
+			}
+			if (size.equalsIgnoreCase("medium")) {
+				makeHouseMedium();
+			}
+			if (size.equalsIgnoreCase("large")) {
+				makeHouseLarge();
+			}
 		}
 
 	}
-	void makeHouseSmall(String size){
-		if (0==0) {
-			rob.move(60);
-			rob.turn(60);
-			rob.move(30);
-			rob.turn(60);
-			rob.move(30);
-			rob.turn(60);
-			rob.move(60);
-		}
-		}
-	void makeHouseMedium(String size){
-		if (0==0) {
-			rob.move(120);
-			rob.turn(60);
-			rob.move(30);
-			rob.turn(60);
-			rob.move(30);
-			rob.turn(60);
-			rob.move(120);
-		}
-		}
-	void makeHouseLarge(String size){
-		if (0==0) {
-			rob.move(250);
-			rob.turn(60);
-			rob.move(30);
-			rob.turn(60);
-			rob.move(30);
-			rob.turn(60);
-			rob.move(250);
-		}
-		}
 
-}
+	void makeHouseSmall() {
+		rob.move(60);
+		rob.turn(roof);
+		rob.move(30);
+		rob.turn(y);
+		rob.move(30);
+		rob.turn(roof);
+		rob.move(60);
+		rob.setPenColor(25, 98, 1);
+		rob.turn(-90);
+		rob.move(40);
+		rob.turn(-90);
+		rob.setPenColor(0, 0, 0);
+	}
+
+	void makeHouseMedium() {
+
+		rob.move(120);
+		rob.turn(roof);
+		rob.move(30);
+		rob.turn(y);
+		rob.move(30);
+		rob.turn(roof);
+		rob.move(120);
+		rob.setPenColor(25, 98, 1);
+		rob.turn(-90);
+		rob.move(40);
+		rob.turn(-90);
+		rob.setPenColor(0, 0, 0);
+	}
+
+	void makeHouseLarge() {
+
+		rob.move(250);
+		rob.turn(roof);
+		rob.move(30);
+		rob.turn(y);
+		rob.move(30);
+		rob.turn(roof);
+		rob.move(250);
+		rob.setPenColor(25, 98, 1);
+		rob.turn(-90);
+		rob.move(40);
+		rob.turn(-90);
+		rob.setPenColor(0, 0, 0);
+	}
+
+	void red() {
+		rob.setPenColor(255, 0, 0);
+	}
+
+	void orange() {
+		rob.setPenColor(255, 154, 3);
+	}
+
+	void yellow() {
+		rob.setPenColor(255, 247, 0);
+	}
+
+	void green() {
+		rob.setPenColor(0, 255, 0);
+	}
+
+	void blue() {
+		rob.setPenColor(0, 0, 255);
+	}
+
+	void purple() {
+		rob.setPenColor(186, 0, 255);
+	}
+	void pointyRoof() {
+		roof = 60;
+		int y = 60;
+	}
+	void flatRoof() {
+		roof = 90;
+		int y = 0;
+	}
+	}
+	
+
