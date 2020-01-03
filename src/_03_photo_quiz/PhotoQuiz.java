660c383a86dff014b3d370dcedc8b3e4893900ad
League Student <league@iMac-9.attlocal.net>
2020-01-02 18:28:30 -0800

    :sunglasses:

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -33,21 +33,25 @@ String url = "https://i.kym-cdn.com/photos/images/original/001/590/564/aff.jpg";
 		// 3. use the "createImage()" method below to initialize your Component
 Component c = createImage(url);
 		// 4. add the image to the quiz window
-quizWindow.add(c);
+//quizWindow.add(c);
 		// 5. call the pack() method on the quiz window
 quizWindow.pack();
 		// 6. ask a question that relates to the image
-JOptionPane.showInputDialog(null, "What is the character in the image?");
+String answer = JOptionPane.showInputDialog(null, "What is the character in the image?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if (answer.equalsIgnoreCase("Shrek Wazowski")) {
+	System.out.println("CORRECT");
+}
 		// 8. print "INCORRECT" if the answer is wrong
-
+else {
+	System.out.println("INCORRECT");
+}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
 
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+String url_2 = "https://pm1.narvii.com/6837/9d2753a6407e87043afbb79d5415ff6f0c78b30bv2_hq.jpg";
 		// 11. add the second image to the quiz window
 
 		// 12. pack the quiz window
