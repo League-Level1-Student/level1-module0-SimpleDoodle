48cc80b48bbfe2f32edafa58b84abbdaea042cbb
League Student <league@iMac-24.attlocal.net>
2020-02-06 18:30:37 -0800

    :tv:

--- src/_10_cuteness_tv/CutenessTV.java
+++ src/_10_cuteness_tv/CutenessTV.java
@@ -1,5 +1,30 @@
 package _10_cuteness_tv;
 
-public class CutenessTV {
+import java.net.URI;
 
+public class CutenessTV {
+public void start() {
+	
+	
+}
+void showDucks() {
+    playVideo("https://www.youtube.com/watch?v=MtN1YnoL46Q");
+}
+
+void showFrog() {
+    playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds");
+}
+
+void showNothingSuspicious() {
+    playVideo("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+}
+
+void playVideo(String videoID) {
+    try {
+         URI uri = new URI(videoID);
+         java.awt.Desktop.getDesktop().browse(uri);
+    } catch (Exception e) {
+         e.printStackTrace();
+    }
+}
 }
